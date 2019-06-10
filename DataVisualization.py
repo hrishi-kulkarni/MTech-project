e5dcb31a1e9c785f985ec69b6d490dddadb2b042
Hrishikesh Kulkarni <40865018+hrishi-kulkarni@users.noreply.github.com>
2019-06-10 09:20:00 +0530

    Add files via upload

--- DataVisualization.py
+++ DataVisualization.py
@@ -1,6 +1,24 @@
-import pandas_profiling
+import pymysql
 import pandas as pd
-df=pd.read_csv('operator.csv',names=['id','name','login_time','opr_time','boom','bucket'],header=None)
+import matplotlib.pyplot as plt
+import pandas_profiling
+import seaborn as sns
+
+QUERY='SELECT * FROM operator;'
+db=pymysql.connect(host='localhost',user='root',passwd='',db='operatorpersonalization')
+results = pd.read_sql_query(QUERY, db)
+results.to_csv("operator.csv", index=False)
+
+df=pd.read_csv('operator.csv')
 print(df.head())
-profile = pandas_profiling.ProfileReport(df)
-profile.to_file(outputfile="output.html")
+
+#TODO: Require change in SQL Query for new data insertion - o/w divide by zero error
+df['Efficiency']=(df['OperationTime']*100)/df['LoginTime']
+
+sns.set(style="whitegrid")
+ax = sns.barplot(df['Name'], df['Efficiency'])
+fig=ax.get_figure()
+fig.savefig('efficiency.jpg')
+
+fig=df.plot.bar(x='Name',y=['LoginTime','OperationTime'], rot=0).get_figure()
+fig.savefig('time_compare.jpg')
